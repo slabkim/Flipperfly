c8d961e215be7be64118599d433ce063ed49c358
sulthon abdul hakim <ihsanledi@gmail.com>
2024-12-12 21:18:22 +0700

    add

--- Flipperfly/src/flipperfly/Flipperfly.java
+++ Flipperfly/src/flipperfly/Flipperfly.java
@@ -1,4 +1,4 @@
-package Flipperfly.src.flipperfly;
+package flipperfly;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- Flipperfly/src/flipperfly/MenuController.java
+++ Flipperfly/src/flipperfly/MenuController.java
@@ -1,5 +1,23 @@
-package Flipperfly.src.flipperfly;
+package flipperfly;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class MenuController {
-    
+    private void goToScene2(MouseEvent event) {
+
+    }
+    private void exitApp(MouseEvent event) {
+        
+    }
 }
