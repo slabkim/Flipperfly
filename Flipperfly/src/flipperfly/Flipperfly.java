2a2455f2ac9ff5c182c53d39ff1b1a9d3d794be4
meyta <151342867+meytazaskiya@users.noreply.github.com>
2024-12-13 10:23:05 +0700

    Update Flipperfly.java

--- Flipperfly/src/flipperfly/Flipperfly.java
+++ Flipperfly/src/flipperfly/Flipperfly.java
@@ -6,7 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
+public class Flipperfly extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+        Scene scene = new Scene(root, 1280, 720);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Flipperfly");
