88b0445664d211e008cb31773f770ecd9129d5fd
meyta <151342867+meytazaskiya@users.noreply.github.com>
2024-12-13 10:10:52 +0700

    Create Building.java

---
+++ Flipperfly/src/flipperfly/Building.java
@@ -0,0 +1,9 @@
+package fxml;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+public class Building extends GameObject {
+    public static final int BUILDING_HEIGHT = 532;
+    private final Image img;
+    boolean passed = false;
