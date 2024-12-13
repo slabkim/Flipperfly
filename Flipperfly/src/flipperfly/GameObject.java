a1a4272e7ba63f1215cdad2cd043f33e2e30bd64
meyta <151342867+meytazaskiya@users.noreply.github.com>
2024-12-13 10:27:53 +0700

    Update GameObject.java

--- Flipperfly/src/flipperfly/GameObject.java
+++ Flipperfly/src/flipperfly/GameObject.java
@@ -4,3 +4,13 @@ import javafx.scene.canvas.GraphicsContext;
 
 public abstract class GameObject {
     int x, y, width, height;
+
+GameObject(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public abstract void draw(GraphicsContext gc);
+}
