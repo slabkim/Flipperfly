b44e65ebeb75a98221c4672254ac641b086b2a46
meyta <151342867+meytazaskiya@users.noreply.github.com>
2024-12-13 10:12:28 +0700

    Update Building.java

--- Flipperfly/src/flipperfly/Building.java
+++ Flipperfly/src/flipperfly/Building.java
@@ -7,3 +7,8 @@ public class Building extends GameObject {
     public static final int BUILDING_HEIGHT = 532;
     private final Image img;
     boolean passed = false;
+    
+public Building(int x, int y, Image img) {
+        super(x, y, 84, BUILDING_HEIGHT);
+        this.img = img;
+    }
